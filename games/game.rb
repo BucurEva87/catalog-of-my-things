3cc1c67b2e719ca3f32712989cb9641ede0c753a
Tommymarvel <ibironketomiwa4@gmail.com>
2022-12-08 09:47:33 +0100

    Test classes

--- games/game.rb
+++ games/game.rb
@@ -3,6 +3,7 @@ require './item'
 
 class Game < Item
   attr_reader :multiplayer, :last_played_at
+
   def initialize(multiplayer, last_played_at, publish_date)
     super(publish_date)
     @multiplayer = multiplayer
