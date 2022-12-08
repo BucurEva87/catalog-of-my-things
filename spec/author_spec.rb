2103c612032df855234cb9bcdef24ab07f833fb3
Tommymarvel <ibironketomiwa4@gmail.com>
2022-12-08 09:41:13 +0100

    Test classes

---
+++ spec/author_spec.rb
@@ -0,0 +1,16 @@
+require './games/author'
+
+describe Author do
+    it 'instance of author' do
+        auth = Author.new('Marvellous', 'ibironke')
+        expect(auth).to be_an_instance_of Author
+    end
+    it 'first_name equals' do
+        auth = Author.new('Marvellous', 'ibironke')
+        expect(auth.first_name).to eq('Marvellous')
+    end
+    it 'last_name equals' do
+        auth = Author.new('Marvellous', 'ibironke')
+        expect(auth.last_name).to eq('ibironke')
+    end
+end
