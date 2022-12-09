f54fd0cc1eeb92459cb5a60342f67c0d07111cb0
Saeqa Sultani <saeqasultani@gmail.com>
2022-12-09 20:12:55 +0430

    fix linters errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -4,7 +4,7 @@ require './music_album'
 describe Genre do
   context 'when initialized' do
     it 'Should create a genre' do
-      music = MusicAlbum.new('2022-10-01',false)
+      music = MusicAlbum.new('2022-10-01', false)
       genre = Genre.new('Ali')
       genre.add_item(music)
       expect(genre.name).to eq('Ali')
