3a97e9885bc564091c99bcbb7c319198d4714a29
Saeqa Sultani <saeqasultani@gmail.com>
2022-12-09 09:17:44 +0430

    created gener and music-album classes & schema.sql file and implement unit test on gener and music-album classes

---
+++ spec/music_spec.rb
@@ -0,0 +1,12 @@
+require './music-album'
+
+
+describe MusicAlbum do
+    context 'when initialized' do
+      it 'Should create a music album' do
+        music = MusicAlbum.new('2021-10-01', true)
+        expect(music.on_spotify).to eq(true)
+        expect(music.can_be_archived?).to be(false)
+      end
+    end
+  end
