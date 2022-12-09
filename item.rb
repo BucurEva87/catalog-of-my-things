89ce114c8eafef4c812b7d87ab6b3532ab14c996
Eva Lavinia Bucur <bucur.eva87@gmail.com>
2022-12-09 23:54:36 +0200

    Update item.rb

--- item.rb
+++ item.rb
@@ -10,9 +10,8 @@ class Item
     @archived = false
   end
 
-  def genre=(genre)
+  def add_genre(genre)
     @genre = genre
-    genre.items << self unless genre.items.include?(self)
   end
 
   def add_author(author)
