15f47b656a6fe535136ec35296d370a17bf5cb72
Saeqa Sultani <saeqasultani@gmail.com>
2022-12-09 09:27:43 +0430

    fix linters errors

--- spec/genre_spec.rb
+++ spec/genre_spec.rb
@@ -1,14 +1,13 @@
-
 require './genre'
 require './item'
 
 describe Genre do
-    context 'when initialized' do
-      it 'Should create a genre' do
-        genre = Genre.new('Ali')
-        item = Item.new('2022-10-01')
-        genre.add_item(item)
-        expect(genre.name).to eq('Ali')
-      end
+  context 'when initialized' do
+    it 'Should create a genre' do
+      genre = Genre.new('Ali')
+      item = Item.new('2022-10-01')
+      genre.add_item(item)
+      expect(genre.name).to eq('Ali')
     end
   end
+end
